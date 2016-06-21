3156b3b271e4af3d3d208496bdee5a42e7241435
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-06-20 23:16:19 -0400

    Adjusted clockCounter code in main.js. Clock is counting in the console, but is not injecting new values back into index.html

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-var clockcounter = function (){
-	setInterval(countUp, 1000);
-};
+var seconds = Number(document.querySelector(".seconds").innerHTML);
+var minutes = Number(document.querySelector(".minutes").innerHTML);
+var hours = Number(document.querySelector(".hours").innerHTML);
 
 var countUp = function(){
-	var clockNums = document.querySelector(".clocknums");
-	clockNums.innerHTML = Number(clockNums) + 1;
+	if (seconds < 60){
+			seconds = seconds + 1;
+		} else if (minutes < 60) {
+			minutes = minutes + 1;
+			seconds = 00;
+		} else {
+			hours = hours + 1;
+			minutes = 00;
+			seconds = 00;
+		}
 };
 
-clockcounter();
+var clockCounter = window.setInterval(countUp, 300);
