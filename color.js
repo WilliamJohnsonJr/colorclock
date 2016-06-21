685cead63339648a5598935129daca9e37f757ab
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-06-21 01:33:45 -0400

    Restored index.html code to original condition, minus links to color.js and normalize.css. Edited original index.html file using JS to achieve all styling and text change effects.

--- color.js
+++ color.js
@@ -5,6 +5,33 @@ var tensMinutes = Number(document.querySelector(".tensMinutes").innerHTML);
 var hours = Number(document.querySelector(".hours").innerHTML);
 var tensHours = Number(document.querySelector(".tensHours").innerHTML);
 
+document.querySelector(".clockNums").innerHTML = "<span class=\"pound\">#</span><span class=\"tensHours\">0</span><span class=\"hours\">0</span><span class=\"tensMinutes\">0</span><span class=\"minutes\">0</span><span class=\"tensSeconds\">0</span><span class=\"seconds\">0</span>";
+
+var marginShift = function (item) {
+	var item = document.querySelector(item);
+	item.style.marginRight = "0px";
+};
+
+marginShift(".tensHours");
+marginShift(".hours");
+marginShift(".tensMinutes");
+marginShift(".minutes");
+marginShift(".tensSeconds");
+marginShift(".seconds");
+marginShift(".pound");
+
+if (seconds < 2) {
+	document.getElementById("body").style.backgroundColor = "#024542";
+} else if (2 < seconds < 4) {
+	document.getElementById("body").style.backgroundColor = "#024548";
+} else if (4 < seconds < 6) {
+	document.getElementById("body").style.backgroundColor = "#024551";
+} else if (6 < seconds < 8) {
+	document.getElementById("body").style.backgroundColor = "#024542";
+} else if (8 < seconds < 10) {
+	document.getElementById("body").style.backgroundColor = "#024548";
+};
+
 var countUp = function(){
 	if (seconds < 9){
 			seconds = seconds + 1;
